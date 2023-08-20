063bb5d2317fb1e58c0b5779253f0edfe2ba62e4
InnaKara <128441416+InnaKara@users.noreply.github.com>
2023-08-20 10:04:59 +0000

    Deploying to gh-pages from @ InnaKara/goit-js-hw-09@669317cbc6db6cc19227e1ce76b4092caf6e103c 🚀

---
+++ 01-color-switcher.9336aa64.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),r=document.querySelector("body");t.addEventListener("click",(function(){t.setAttribute("disabled",!0),o=setInterval((()=>{r.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3)}));let o="null";e.addEventListener("click",(()=>{clearInterval(o),t.removeAttribute("disabled")}));
+//# sourceMappingURL=01-color-switcher.9336aa64.js.map
