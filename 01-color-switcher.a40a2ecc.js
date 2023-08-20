714061ae9dc90806ac10c52f5ea32c35f417ea22
InnaKara <128441416+InnaKara@users.noreply.github.com>
2023-08-20 02:01:49 +0000

    Deploying to gh-pages from @ InnaKara/goit-js-hw-09@c2933fc3114798813409feee6d7b34343a8c8e8f 🚀

---
+++ 01-color-switcher.a40a2ecc.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),r=document.querySelector("body");t.addEventListener("click",(function(){t.setAttribute("disabled",!0),timerId=setInterval((function(){r.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}),1e3)})),e.addEventListener("click",(function(){t.removeAttribute("disabled"),clearInterval(timerId)}))}();
+//# sourceMappingURL=01-color-switcher.a40a2ecc.js.map
---
+++ 01-color-switcher.b0e42650.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),r=document.querySelector("body");t.addEventListener("click",(function(){t.setAttribute("disabled",!0),timerId=setInterval((()=>{r.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3)})),e.addEventListener("click",(()=>{t.removeAttribute("disabled"),clearInterval(timerId)}));
+//# sourceMappingURL=01-color-switcher.b0e42650.js.map
